052855fc99f733f8fa1fce497d6ae32a1153dbe3
gottn <musab.ahmed1996@gmail.com>
2024-07-21 21:47:21 +0200

    Finished authors route

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
   name: {
@@ -7,4 +8,16 @@ const authorSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("Author", authorSchema);
+authorSchema.pre('remove', function(next) {
+  Book.find({ author: this._id }, (err, books) => {
+    if (err) {
+      next(err);
+    } else if (books.length > 0) {
+      next(new Error('This author has books still'));
+    } else {
+      next();
+    }
+  });
+});
+
+module.exports = mongoose.models.Author || mongoose.model('Author', authorSchema);
